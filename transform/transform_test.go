26044afed6e17ddabae68924ca75dadb09a347b9
lamrin13 <patelnirmal13595@gmail.com>
2023-08-13 10:21:40 -0400

    restructure test files

--- transform/transform_test.go
+++ transform/transform_test.go
@@ -28,30 +28,30 @@ func BenchmarkReshape(b *testing.B) {
 }
 
 func TestReshape(t *testing.T) {
-	paths, err := filepath.Glob(filepath.Join("testdata", "*.mapping"))
+	entries, err := os.ReadDir("./testdata")
 	if err != nil {
-		t.Fatal(err)
+		log.Fatal(err)
 	}
+	for _, testDir := range entries {
 
-	for _, path := range paths {
-		_, filename := filepath.Split(path)
-		testname := filename[:len(filename)-len(filepath.Ext(path))]
+		testname := testDir.Name()
 
 		// Each path turns into a test: the test name is the filename without the
 		// extension.
 		t.Run(testname, func(t *testing.T) {
+			path := filepath.Join("testdata", testname, testname+".mapping")
 			source, err := os.ReadFile(path)
 			if err != nil {
 				t.Fatal("error reading config file:", err)
 			}
 
-			goldenfile := filepath.Join("testdata", testname+".golden")
+			goldenfile := filepath.Join("testdata", testname, testname+".golden")
 			golden, err := os.ReadFile(goldenfile)
 			if err != nil {
 				t.Fatal("error reading golden file:", err)
 			}
 
-			originalfile := filepath.Join("testdata", testname+".input")
+			originalfile := filepath.Join("testdata", testname, testname+".input")
 			original, err := os.ReadFile(originalfile)
 			if err != nil {
 				t.Fatal("error reading original file:", err)
